9733d7ff1675edfe622882978909d7bd540dc681
majkl zumberi <majklzumberi00@gmail.com>
2021-03-20 11:55:34 +0100

    feat(initial): :tada: initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app class="layout">
+    <v-app-bar app flat>
+      <v-toolbar-title>Gridsome</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+</style>
