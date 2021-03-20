47bd4e70eeacea3e26a07a8b74901ff8c42b6a3b
majkl zumberi <majklzumberi00@gmail.com>
2021-03-20 22:44:27 +0100

    feat(index Page): :construction: fix style & responsiveness first section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,13 +3,20 @@
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <v-container fluid style="margin:0;padding:0" >
-    <v-layout row justify-space-around class=" first-section">
-      <v-flex xs12 sm5 md5 class="pa-14">
-        <h1>ONLINE LEARNING</h1>
-          <h2>Learn Everyday</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <v-layout row justify-space-around class="first-section">
+      <v-flex xs12 sm6 md5 class="pa-14">
+        <span class="h5 font-weight-regular">ONLINE LEARNING</span>
+          <h1 class="font-weight-regular">Learn <span class="font-weight-bold">Everyday</span></h1>
+          <div class="text-wrapper-card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <v-btn
+            color="brown mt-9"
+            dark >
+            Normal Button
+          </v-btn>
       </v-flex>
-      <v-flex xs12 sm7 md7 class="image-container">
+      <v-flex xs12 sm6 md7 class="image-container">
       </v-flex>
     </v-layout>
     </v-container>
@@ -26,13 +33,29 @@ export default {
 </script>
 
 <style>
+.text-wrapper-card{
+  padding: 1em 2em;
+  line-height: 2rem;
+  max-width: 20rem;
+  min-width: 15rem;
+  background-color: #c1a29a;
+  border-radius: 10px;
+  color: white;
+  text-align: left;
+}
+h1,h5{
+  font-size: 3.4rem;
+}
+h1,h2,p,span{
+  font-family: "nunito";
+}
 .home-links a {
   margin-right: 1rem;
 }
 .first-section{
    background: rgb(255,	224,	215);
   background: linear-gradient(135deg, rgba(255,	224,	215) 24%, rgba(255,	224,	215) 84%); 
-  height: 75vh;
+  height: 80vh;
   
 }
 .image-container{
@@ -41,10 +64,11 @@ export default {
   width: 900px;
   height: auto;
   position: relative;
-  mix-blend-mode: luminosity;
+  /* mix-blend-mode: luminosity; */
+  filter: hue-rotate(27deg);
   background-position: center;
   transform:scaleX(-1);
   border-radius: 5px;
-  height: 75vh;
+  height: 80vh;
 }
 </style>
