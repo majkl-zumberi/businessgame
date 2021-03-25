73c988afe058014b45d7f676c6887bbef80e96d1
majkl zumberi <majklzumberi00@gmail.com>
2021-03-25 15:14:49 +0100

    refactor(index Page): :sparkles: add content & footer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,19 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <v-container fluid style="margin:0;padding:0;ovewflow-x:hidden;" >
+    <v-container fluid style="margin:0;padding:0;" >
     <v-layout row justify-space-around class="first-section">
       <v-flex xs12 sm6 md5 justify-center class="pa-14 pa-md-16" style="background: inherit;">
         <div class="d-lg-flex flex-lg-column align-lg-center" style="height:31.25em;">
           <span class="h5 font-weight-regular">ONLINE LEARNING</span>
-          <h1 class="font-weight-regular">Learn <span class="font-weight-bold">Everyday</span></h1>
+          <h1 class="font-weight-regular mb-md-12">Learn <span class="font-weight-bold">Everyday</span></h1>
           <div class="text-wrapper-card">
-            <p class="font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p class="font-weight-bold">Scopri cosa significa essere un vero Marketer attraverso i nostri corsi dedicati al mondo marketing! <br> Ti aspettiamo!</p>
           </div>
           <v-btn
             color="brown mt-9"
             dark >
-            Normal Button
+            compila il questionario
           </v-btn>
         </div>
       </v-flex>
@@ -37,29 +36,32 @@
         </v-col>
         <v-col cols="12" xs="12" sm="12" md="5" xl="6" offset-md="2" offset-xl="1" class="pa-6 pa-md-16">
           <div class="image-circle-container">
-            <p class="text-h6 text-md-h5 headline blue-grey--text text--darken-3 font-weight-light">Get access to high quality learning!</p>
-            <h1 class="font-weight-regular text-h4 text-md-h3">Join online courses <span class="font-weight-bold">today</span></h1>
-            <p class="text-wrapper-card font-weight-bold">Etiam velit purus, vehicula quis imperdiet dignissim, convallis vitae nisl. Nullam facilisi. Pellentesque non pulvinar justo. Nullam eget neque ut tellus ornare ullamcorper ulla ambco mattis for nulla glavrida egesed!</p>
-            <v-btn
-              color="brown mt-9"
-              dark >
-              Read more
-            </v-btn>
+            <p class="text-h6 text-md-h5 headline blue-grey--text text--darken-3 font-weight-light">Ottieni l'accesso ad un apprendimento di alta qualità!</p>
+            <h1 class="font-weight-regular text-h4 text-md-h3">Iscriviti ai corsi online <span class="font-weight-bold">oggi</span></h1>
+            <p class="mt-2 text-wrapper-card font-weight-bold">Sviluppa nuove competenze per un mondo digitale, scopri gli aspetti fondamentali del marketing digitale grazie ai nostri corsi tutti creati da formatori Professionisti</p>
+             <v-btn
+            class="px-2 mb-sm-2 mb-2"
+            outlined
+            color="brown"
+            :width="$vuetify.breakpoint.xs?'100%':null"
+          >
+            Scopri tutti i corsi
+          </v-btn>
           </div>
         </v-col>
       </v-row >
 
       <v-row no-gutters align="center" class="pa-14 pa-md-16" style="background-color:#ffe0d7;">
         <v-col cols="12"  xs="12" sm="9" md="4" lg="3" offset-sm="3" offset-md="2" offset-lg="3">
-          <h1 class="font-weight-regular ">Train Your Brain Today!</h1>
-          <p class="text-wrapper-card">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ​reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <h1 class="font-weight-regular ">Mettiti alla prova!</h1>
+          <p class="text-wrapper-card">Testa la tua digital marketing knowledge eseguendo un semplice quiz</p>
            <v-btn
             class="px-2 mb-sm-2 mb-2"
             outlined
             color="brown"
             :width="$vuetify.breakpoint.xs?'100%':null"
           >
-            All courses
+            avvia quiz
           </v-btn>
         </v-col>
 
@@ -73,6 +75,39 @@
         </v-col>
       </v-row>
       
+      <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="brown lighten-3 black--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Talenti4Future nasce per assecondare il desiderio di ognuno di essere protagonista e per stimolarti a fare della tua vita qualcosa di grande. È proprio il desiderio ciò che prendiamo a cuore all’inizio del tuo percorso, ancor prima delle tue competenze, per aiutarti a raggiungere i tuoi obiettivi personali, accademici e professionali.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>TALENTI4FUTURE</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
     </v-layout>
     
     </v-container>
@@ -85,7 +120,17 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Homepage'
+  },
+  data(){
+    return {
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }
   }
 }
 </script>
